ff850e8b01aaf524da2e41a925592a54bebf77f2
Casey Key <casey.key@protonmail.com>
2021-08-09 13:49:02 -0700

    Turns working with some styling

--- src/App.js
+++ src/App.js
@@ -13,12 +13,13 @@ class App extends Component {
       margin: 15,
       numDice: 2,
       sides: 6,
-      rollTime: 2,
+      rollTime: 1,
       faceColor: '#FF00AC',
       dotColor: '#1eff00',
       diceTotal: '...',
       rolling: false,
       rollCount: 0,
+      style: {float: 'left', marginRight: '5em'},
     }
     this.rollDone = this.rollDone.bind(this);
     this.rollAll = this.rollAll.bind(this);
@@ -34,20 +35,46 @@ class App extends Component {
   }
   
   render() {
-    let style;
-    if(this.state.rollCount === 1) {
-      style = {float: 'right'}
+    let stage;
+    if (this.state.rollCount === 0)
+      stage ="The Comeout";
+    else {
+      let rollTotal = parseInt(this.state.diceTotal);
+
+      if ( rollTotal === 7 || rollTotal === 11) {
+        console.log("Shooter wins!");
+        stage = "Shooter Wins! Take pot, then keep rolling.";
+        this.state.rollCount = 0;
+      } else if ( rollTotal === 2 || rollTotal === 3 || rollTotal === 12) {
+        console.log("Shooter loses!");
+        stage = "Shooter loses! Pass the dice.";
+        if(this.state.style['float'] === 'right') {
+          this.setState({style: {...this.state.style, 'float': 'left'}});
+        }
+        else
+          this.setState({style: {...this.state.style, 'float': 'right'}});
+        this.state.rollCount = 0;
+      } else {
+        stage = "Point is " + rollTotal;
+      }
     }
+
     return (
-      <div style={style}>
+      <div 
+        style={this.state.style}
+      >
         <h4>Roll {this.state.rollCount}</h4>
+        <h1>{stage}</h1>
         <ReactDice
           {...this.state}
           rollDone={this.rollDone}
           ref={dice => this.reactDice = dice}
         />
 
-        <button onClick={this.rollAll}>
+        <button 
+          onClick={this.rollAll}
+          disabled={this.state.rolling}
+        >
           Roll All
         </button>
         
