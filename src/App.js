f0adcc338e6fe3067293fb44efd1bc6b65292fc1
Casey Key <casey.key@protonmail.com>
2021-08-09 15:22:46 -0700

    added more to state, bug with winning moving sides exists

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      outline: false,
+      outline: true,
       outlineColor: '#0000000',
       dieSize: 60,
       disableIndividual: true,
@@ -14,15 +14,17 @@ class App extends Component {
       numDice: 2,
       sides: 6,
       rollTime: 1,
-      faceColor: '#FF00AC',
-      dotColor: '#1eff00',
+      faceColor: '#663399',
+      dotColor: '#F5BD02',
       diceTotal: '...',
       rolling: false,
       rollCount: 0,
       style: {float: 'left', marginRight: '5em'},
-      stage: "The Comeout",
+      stage: "The Comeout", // Either the Comeout or the Point number (2 to 12)
     }
     this.rollDone = this.rollDone.bind(this);
+    this.shooterWins = this.shooterWins.bind(this);
+    this.shooterLoses = this.shooterLoses.bind(this);
     this.rollAll = this.rollAll.bind(this);
   }
 
@@ -34,33 +36,51 @@ class App extends Component {
     // First roll of the game, after deciding who rolls first
     // For now, the left side is the Shooter aka Player 1
     if (this.state.stage === "The Comeout") {
-      // Shooter wins
+      // Shooter wins on The Comeout
       if ( rollTotal === 7 || rollTotal === 11) {
-        this.setState({message: "Shooter Wins! Take pot, then keep rolling."});
-        this.state.rollCount = 0;
+        this.shooterWins();
       } 
-      // Shooter loses
+
+      // Shooter loses on The Comeout
       else if ( rollTotal === 2 || rollTotal === 3 || rollTotal === 12) {
-        message = "Shooter loses! Pass the dice.";
-        // If Player 2 lost, return to Player 1
-        if(this.state.style['float'] === 'right') {
-          this.setState({style: {...this.state.style, 'float': 'left'}});
-        }
-        // If Shooter loses (Player 1), float the dice to Player 2
-        else
-          this.setState({style: {...this.state.style, 'float': 'right'}});
-        this.state.stage = "The Comeout";
+        this.shooterLoses();
       } 
+
       // Point set
       else {
-        stage = "Point is " + rollTotal;
+        this.setState({message: "Point is " + rollTotal, stage: rollTotal})
       }
-    } 
+    }
+
     // Point set for game
     else {
-
+      // Shooter wins
+      if ( rollTotal === this.state.stage ) {
+        this.shooterWins();
+      }
+      // Shooter loses, "7 out"
+      else if ( rollTotal === 7 ) {
+        this.shooterLoses();
+      }
     }
   }
+
+  shooterWins() {
+    this.setState({message: "Shooter Wins! Take pot, then keep rolling.", rollCount: 0});
+  }
+
+  shooterLoses() {
+    this.setState({message: "Shooter loses! Pass the dice.", rollCount: 0});
+
+    // If Player 2 lost, return to Player 1
+    if(this.state.style['float'] === 'right') {
+      this.setState({style: {...this.state.style, 'float': 'left'}});
+    }
+    // If Shooter loses (Player 1), float the dice to Player 2
+    else
+      this.setState({style: {...this.state.style, 'float': 'right'}});
+  }
+
 
   rollAll() {
     this.reactDice.rollAll();
@@ -75,7 +95,8 @@ class App extends Component {
         style={this.state.style}
       >
         <h4>Roll {this.state.rollCount}</h4>
-        <h1>{stage}</h1>
+        <h1>{this.state.message}</h1>
+        <h2>{this.state.stage}</h2>
         <ReactDice
           {...this.state}
           rollDone={this.rollDone}
